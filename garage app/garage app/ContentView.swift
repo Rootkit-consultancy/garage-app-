03bc55d1715268596590dbf831bdc91d2f39d5ec
Rootkit-consultancy <rootkitconsultancy@gmail.com>
2026-01-07 20:09:19 +0530

    qwerty

---
+++ garage app/garage app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  garage app
+//
+//  Created by SHIVAM KUMAR GOYAL on 07/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ garage app/garage app/garage_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  garage_appApp.swift
+//  garage app
+//
+//  Created by SHIVAM KUMAR GOYAL on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct garage_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
